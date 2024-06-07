d955f79ef643681407417a67e42352ada6320eea
Anthony Garland <garland3a@live.com>
2024-06-07 14:09:15 -0600

    ui update

--- C1_webserver.py
+++ C1_webserver.py
@@ -1,5 +1,6 @@
 # conda activate waterproject
 import platform
+import time
 computer_name = platform.node()
 print(computer_name)
 if computer_name!="DESKTOP-7DC3UA9":
@@ -8,20 +9,22 @@ else:
     from myfake import GPIO
 
 
-import time
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 import json
 import os
 
+log_file = "log.csv"
+
 
 class Device:
-    def __init__(self, name, pin):
+    def __init__(self, name, pin, location):
         self.name = name
         self.pin = pin
+        self.location = location
         self.state = False
         self.turn_on_times=[]
         self.turn_off_times=[]
@@ -29,13 +32,20 @@ class Device:
     def toggle(self):
         self.state = not self.state
         GPIO.output(self.pin, GPIO.LOW if self.state else GPIO.HIGH)
+        # append the name, pin, location, state and time to the log file, put quotes around the name and location, use the full date and time
+        # seperate with commas
+        with open(log_file, "a") as f:
+            f.write(f'"{self.name}", {self.pin}, "{self.location}", {self.state}, {time.strftime("%Y-%m-%d %H:%M:%S")}\n')
+            # f.write(f'"{self.name}", {self.pin}, "{self.location}", {self.state}, {time.strftime("%Y-%m-%d %H:%M:%S")}\n')
+            
 
 devices = [
-    Device("power_cable_solenoid", 12),
-    Device("valve_1", 25),
-    Device("valve_2", 23),
-    Device("valve_3", 18)
+    Device("power_cable_solenoid", 12, ""),
+    Device("valve_1", 25, "Middle Garden"),
+    Device("valve_2", 23, "Rock Wall Garden"),
+    Device("valve_3", 18, "Grape Garden"),
 ]
+
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
@@ -80,9 +90,15 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+def common_return(request):
+    # get the  day of the week  and time to the second and add it to the context
+    current_time = time.strftime("%A %H:%M:%S")
+    return templates.TemplateResponse("index.html", {"request": request, "devices": devices, "current_time": current_time})
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "devices": devices})
+    return common_return(request)
+    
 
 @app.post("/toggle/{pin}")
 async def toggle_device(pin: int, request: Request):
@@ -90,5 +106,22 @@ async def toggle_device(pin: int, request: Request):
         if device.pin == pin:
             device.toggle()
             break
-    return templates.TemplateResponse("index.html", {"request": request, "devices": devices})
-#  uvicorn C1_webserver:app --reload
+    #  send the user to index function
+    return common_return(request)
+    
+# show log
+@app.get("/log_download")
+async def log_download():
+    return FileResponse(log_file, filename='log.csv')
+
+
+
+@app.get("/schedule", response_class=HTMLResponse)
+async def schedule(request: Request):
+    return templates.TemplateResponse("set_schedule.html", {"request": request, "devices": devices})    
+
+
+
+
+
+
